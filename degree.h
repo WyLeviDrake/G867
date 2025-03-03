81ec333c9b4b9689be17b5311f20738acaaf5bff
WyLeviDrake <LeviDrakeCS@gmail.com>
2025-03-02 21:26:32 -0700

    Correcting saving error

---
+++ degree.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+enum DegreeProgram {
+    SECURITY,
+    NETWORK,
+    SOFTWARE
+};
+
+static const std::string degreeProgramStrings[] = {
+    "SECURITY",
+    "NETWORK",
+    "SOFTWARE"
+};
--- roster.h
+++ roster.h
@@ -3,10 +3,11 @@
 
 class Roster {
     private:
-      Student* classRosterArray[5];
+        int index = -1;
+        Student* classRosterArray[5];
     
     public:
-        void parse();
+        void parse(string tableRow);
         void add(string studentId, string firstName, string lastName, string email, int age, int courseLength1, int courseLength2, int courseLength3, DegreeProgram degreeProgram);
         void remove(string studentId);
 
